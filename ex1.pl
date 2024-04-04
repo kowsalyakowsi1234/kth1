d5b9dba0d6bfb2a763bb556f61dcaf8414568d22
kowsalyakowsi1234 <146157195+kowsalyakowsi1234@users.noreply.github.com>
2024-04-04 10:24:20 +0530

    Add files via upload

---
+++ ex1.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+print"enter a string:";
+my$string=<STDIN>;
+chomp($string);
+if($string eq reverse $string)
+{
+	print"the string is a `palindrome\n";
+}
+else
+{
+	print"the string is not a palindrome\n";
+}
+	
